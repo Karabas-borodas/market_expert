a2f24d45e80055527b5cc152abbeed181a48c808
MIhail <zoood3.14@gmail.com>
2026-09-14 19:21:57 +0300

    ADD: hardcode config path

--- src/internal/config/config.go
+++ src/internal/config/config.go
@@ -8,19 +8,24 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
+// FIX: захардкожен путь конфига чтол бы не вызвать каждый раз:
+const configPath = "config/docker.yaml"
+
 type Config struct {
 	Env        string `yaml:"env" env-default:"local"`
 	HTTPserver `yaml:"http_server"`
 }
 
 type HTTPserver struct {
-	Address       string        `yaml: "address" env-default:"localhost:2005"`
+	Address       string        `yaml: "address" env-default:"localhost:20005"`
 	Timeout       time.Duration `yaml:"timeout" env-default:"10s"`
 	Iddle_timeout time.Duration `yaml:"iddle_timeout" env-default:"80s"`
 }
 
 func MustLoad() Config {
-	configPath := os.Getenv("CONFIG_PATH")
+	//FIX: захардкожен путь конфига чтол бы не вызвать каждый раз:
+	//FIX:CONFIG_PATH=config/docker.yaml go run cmd/marketplace/main.go
+	// configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
 		log.Fatalf("config file is not set")
 	}
